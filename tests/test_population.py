19aa7e494f6a9dc4b2222855e4cc0d84b6a7a9bd
MatiashAnastasia <matyash21341@gmail.com>
2026-04-10 16:00:27 +0300

    add sample file for tests

--- tests/test_population.py
+++ tests/test_population.py
@@ -7,3 +7,15 @@ def sample_data():
         "Ukraine": {2000: 49000000, 2010: 45900000, 2020: 41000000},
         "Poland": {2000: 38600000, 2010: 38500000, 2020: 37800000},
     }
+
+@pytest.fixture
+def sample_file(tmp_path):
+    content = (
+        "Ukraine, 2000, 49000000\n"
+        "Ukraine, 2010, 45900000\n"
+        "Poland, 2000, 38600000\n"
+        "Poland, 2010, 38500000\n"
+    )
+    test_file = tmp_path / "test_population.txt"
+    test_file.write_text(content, encoding="utf-8")
+    return str(test_file)
